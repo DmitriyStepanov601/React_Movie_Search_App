f45fbe0ce7bf3767441fdf357eed29b956f35aac
GreyStone601 <dmitr97stepanov@gmail.com>
2022-07-12 13:41:08 +0300

    fetching data with api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Movie from "./components/Movie";
+
+const API_KEY = "k_k8zws5y8";
+const FETURED_API ="https://imdb-api.com/API/AdvancedSearch/${API_KEY}/?title=inception";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [movies, setMovies] = useState([]);
+
+	const searchMovies = async () => {
+		const res = await fetch(FETURED_API);
+		const data = await res.json();
+		console.log(data);
+		setMovies(data.results)
+	};
+
+	useEffect(() => {
+		searchMovies()
+	}, []);
+
+	return (
+		<div className="movies">
+			{ movies.map(movie => (
+				<Movie 
+					key = { movie.id } 
+					data = { movie }
+				/>
+			))}
+		</div>
+	);
 }
 
 export default App;
