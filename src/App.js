b72ef7791e95a669ad1780254f344d2cd16aa2ba
GreyStone601 <dmitr97stepanov@gmail.com>
2022-07-15 18:46:06 +0300

    making changes with app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Movie from "./components/Movie";
 
 const FEATURED_API = 
-	"https://imdb-api.com/API/AdvancedSearch/k_k8zws5y8?user_rating=1.0,10&groups=top_250&count=50&sort=alpha,asc";
+	"https://imdb-api.com/API/AdvancedSearch/k_k8zws5y8?user_rating=1.0,10&groups=top_250&count=100&sort=alpha,asc";
 	
 const SEARCH_API = "https://imdb-api.com/API/AdvancedSearch/k_k8zws5y8/?title=";
 
@@ -39,6 +39,7 @@ function App() {
 	return (
 		<>	
 			<header className="flex justify-end p-4 bg-black">
+				<h3 className=" text-amber-300 font-bold p-2">Movie Search(IMDB)</h3>
 				<form onSubmit={handleOnSubmit}>
 					<input className="bg-transparent p-2 text-base text-amber-300 
 					focus:outline-offset-0 placeholder:text-amber-300" 
@@ -49,9 +50,16 @@ function App() {
 					/>
 				</form>
 			</header>	
-			<div className='movies'>
+			<div className="flex gap-x-10 justify-center bg-amber-400">
 				{ movies.map(movie => (
-					<Movie key={movie.id} {... movies}/>
+					<Movie 
+						key = {movie.id} 
+						title = {movie.title} 
+						image = {movie.image} 
+						genres = {movie.genres} 
+						plot = {movie.plot} 
+						imDbRating = {movie.imDbRating} 
+					/>
 				))}
 			</div>
 		</>
